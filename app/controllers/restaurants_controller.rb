de90a6701025f6e25de1624281f69727afd75769
Matthew Bates <matbat99@yahoo.com>
2019-05-15 14:50:33 +0200

    reviews working

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,14 +1,30 @@
 class RestaurantsController < ApplicationController
   def index
+    @restaurants = Restaurant.all
   end
 
   def show
+    @message = params[:notice]
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant Created!'
+    else
+      render :new
+    end
   end
 
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
 end
